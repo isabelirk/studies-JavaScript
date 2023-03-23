d360ae5ed8182de9422104ce7cb01721e06d7e57
isabelirk <isabelireik2@gmail.com>
2023-03-23 19:05:45 -0300

    aula 2

---
+++ Aula2/index.js
@@ -0,0 +1,16 @@
+let nome = "Isabeli";
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+//window.alert(nome);
+
+//let number = window.prompt("Digite um número:");
+//console.log(number);
+
+//let confirmacao = window.confirm("Você confirma essa ação?");
+//console.log(confirmacao);
+
+// O window é um objeto global, então é possível usa-lo sem referenciar ele diretamente
+let confirmacao = confirm("Você confirma essa ação?");
+console.log(confirmacao);

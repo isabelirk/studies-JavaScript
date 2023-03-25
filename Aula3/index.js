4815614a0f494e63fa3b7f842b6c8be69ea8a338
isabelirk <isabelireik2@gmail.com>
2023-03-25 15:03:36 -0300

    condicionais

---
+++ Aula3/index.js
@@ -0,0 +1,12 @@
+let age = window.prompt("Digite uma idade:");
+
+if (age >= 18) {
+  alert("Maior de idade.");
+} else if (age <= 12) {
+  alert("Criança.");
+} else {
+  alert("Menor de idade.");
+}
+
+const resultado = 6 === 6 ? "Verdadeiro" : "Falso";
+console.log(resultado);

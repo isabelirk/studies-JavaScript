7dfe9c85241231338f3090811c2188a6a74d22ec
isabelirk <isabelireik2@gmail.com>
2023-03-27 11:20:32 -0300

    exercício 7 com html

---
+++ Exercicio7(versionWithHTML)/index.js
@@ -0,0 +1,33 @@
+let option = "";
+
+do {
+  option = prompt(
+    "Seja bem-vindo(a)!\n" +
+      "\nEscolha uma das opções abaixo:" +
+      "\n1 - Opção Um" +
+      "\n2 - Opção Dois" +
+      "\n3 - Opção Três" +
+      "\n4 - Opção Quatro" +
+      "\n5 - Encerar"
+  );
+
+  switch (option) {
+    case "1":
+      alert("Você escolheu a opção 1.");
+      break;
+    case "2":
+      alert("Você escolheu a opção 2.");
+      break;
+    case "3":
+      alert("Você escolheu a opção 3.");
+      break;
+    case "4":
+      alert("Você escolheu a opção 4.");
+      break;
+    case "5":
+      alert("Encerrando o programa...");
+      break;
+    default:
+      alert("Entrada inválida! Escolha uma das cinco opções.");
+  }
+} while (option !== "5");

0614189d270ca85f304f50f929d120ed7a03ce15
isabelirk <isabelireik2@gmail.com>
2023-03-30 16:48:57 -0300

    aula 4

---
+++ Aula4/index.js
@@ -0,0 +1,35 @@
+const fruits = ["Maça", "Abacaxi", "Morango", "Banana", "Melão", "Jabuticaba"];
+console.log(fruits);
+
+// Adicionando um elemento no final do array e retorna o tamanho atual
+fruits.push("Pêssego");
+console.log(fruits);
+
+// Adicionando um elemento no início do array e retorna o tamanho atual
+fruits.unshift("Kiwi");
+console.log(fruits);
+
+// Remove o último elemento do array e devolve o elemento removido
+let lastElement = fruits.pop();
+console.log(lastElement);
+console.log(fruits);
+
+// Remove o primeiro elemento do array e devolve o elemento removido
+lastElement = fruits.shift();
+console.log(lastElement);
+console.log(fruits);
+
+// Pesquisa se o elemento está presente no array e retorna true ou false
+const contain = fruits.includes("Jabuticaba");
+console.log(contain);
+
+// Pesquisa em qual índice o elemento está presente no array
+const index = fruits.indexOf("Jabuticaba");
+console.log(index);
+
+// "Corta" ou copia uma parte do array
+const copy = fruits.slice(0, 3);
+const contrary = fruits.slice(-3); // quando negativo, conta os índices do final para o início do array
+console.log(copy);
+console.log(contrary);
+console.log(fruits); // o array original não é modificado

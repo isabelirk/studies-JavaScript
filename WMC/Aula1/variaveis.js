ed538289d286ac6723af89c8f319ee16a9b697a1
isabelirk <isabelireik2@gmail.com>
2023-04-15 20:12:15 -0300

    aula 1

---
+++ WMC/Aula1/variaveis.js
@@ -0,0 +1,26 @@
+// var ->
+var n1 = 10;
+var n2 = 20;
+var soma = n1 + n2;
+console.log(soma);
+
+// let -> declaração de uma variavel local de escopo do bloco opcionalmente inicializando com algum valor
+let saudacao = "Olá mundo!";
+
+if (true) {
+  let saudacao = "Estou aprendendo JavaScript!";
+  console.log(saudacao);
+}
+
+console.log(saudacao);
+
+// const -> declaração de uma constante de escopo de bloco de apenas de leitura
+const cidade = "Erechim";
+
+function estado() {
+  const cidade = "Chapecó";
+  console.log(cidade);
+}
+
+estado();
+console.log(cidade);

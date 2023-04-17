e479ee19b352a1548c96c91ac274ed69a2a7747b
isabelirk <isabelireik2@gmail.com>
2023-04-17 12:05:46 -0300

    aula 4

---
+++ WMC/Aula4/operadoresLogicos.js
@@ -0,0 +1,17 @@
+// Operador And && (tende a ser false)
+
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+
+// Operador Or || <- (pipe) (tende a ser true)
+
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+
+// Operador Not !
+console.log(!true); // false
+console.log(!false); // true
